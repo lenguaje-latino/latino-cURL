08f25e2d17ad60b92d0449f28de3e917289210b4
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2017-09-25 14:14:17 -0500

    se actualiza instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,2 +1,3 @@
+apt-get install libssl-dev
 rm CMakeCache.txt
 cmake -DBUILD_CURL_EXE=0 -DENABLE_MANUAL=0 -DBUILDING_LIBCURL=1 -DCURL_STATICLIB=1 -DCMAKE_INSTALL_PREFIX:PATH=/usr . && make all install
